39bdf5e153837e060bcab7d8012409038ccf08bb
sajib9090 <nispap.devil.sajib@gmail.com>
2023-06-10 22:15:49 +0600

    students api

--- index.js
+++ index.js
@@ -26,10 +26,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const usersCollection = client
+    const studentsCollection = client
       .db("globalLinguisticsHubDB")
-      .collection("users");
+      .collection("students");
 
+    //get api
+    app.get("/students", async (req, res) => {
+      const result = await studentsCollection.find().toArray();
+      res.send(result);
+    });
+    //post api
+    app.post("/students", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await studentsCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await studentsCollection.insertOne(user);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
